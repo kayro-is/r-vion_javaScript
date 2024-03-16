b5e6232adbe4012f6f1703b64291d61b6d8e1366
kayro-is <luffykosuki@gmail.com>
2024-03-17 00:32:47 +0100

    Exécution Parallèle avec Promise.all

--- fetchData.js
+++ fetchData.js
@@ -9,3 +9,16 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
 
 .then(post => console.log(post)) // afficher le poste récupéré.
 . catch(error => console.error('Erreur:', error));
+
+
+// lancer plusieur requêtes en paralléle et attendre que toutes soient résolues.
+
+Promise.all([
+    fetch('https://jsonplaceholder.typicode.com/posts/1'),
+    fetch('https://jsonplaceholder.typicode.com/posts/2'),
+    fetch('https://jsonplaceholder.typicode.com/posts/3')
+])
+
+.then(response => Promise.all(response.map(res => res.json()))) // Transformer toutes les reponse en json.
+.then(posts => console.log(posts)) // afficher tous les postes récupérés.
+.catch(error => console.error('Erreur lors de la récupération des posts : ', error));
