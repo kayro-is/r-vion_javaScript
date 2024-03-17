e28bd39de03ac4b4b461416ee9540e75b47c363f
kayro-is <luffykosuki@gmail.com>
2024-03-17 13:20:43 +0100

    Création et Ajout Dynamique d'élements

--- dom.js
+++ dom.js
@@ -15,3 +15,39 @@ element.style.color = 'white';
 element.style.border = '1px solid black';
 element.style.padding = '10px';
 element.style.margin = '10px';
+element.style.fontSize = '2rem';
+
+// creation d'un nouvelle element p
+const paragraphe = document.createElement('p');
+paragraphe.textContent = 'ca cest un nouveau paragraphe.'
+
+document.body.appendChild(paragraphe);
+paragraphe.style.backgroundColor ='green';
+paragraphe.style.color ='white';
+paragraphe.style.padding = '10px';
+
+const paragraphe1 = document.createElement('p');
+paragraphe1.textContent = 'ca cest une deuxieme paragraphe.'
+document.body.appendChild(paragraphe1);
+
+paragraphe1.style.backgroundColor ='orange';
+paragraphe1.style.color ='white';
+paragraphe1.style.padding = '10px';
+
+const paragraphe2 = document.createElement('p');
+paragraphe2.textContent = 'ca cest une troissieme paragraphe.'
+
+ document.body.appendChild(paragraphe2);
+
+ paragraphe2.style.backgroundColor ='yellow';
+paragraphe2.style.color ='black';
+paragraphe2.style.padding = '10px';
+
+
+const paragraphe3 = document.createElement('p');
+paragraphe3.textContent = 'ca cest une quatriéme paragraphe';
+
+document.body.appendChild(paragraphe3);
+paragraphe3.style.backgroundColor ='red';
+paragraphe3.style.color ='white';
+paragraphe3.style.padding = '10px';
